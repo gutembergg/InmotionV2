4303c160bcd3a372285336ba30a74f0403742ad2
Fatih-esgi <fatih@fat-e.ch>
2021-11-24 10:09:51 +0100

    fix language button

--- src/components/LanguageSelector/index.tsx
+++ src/components/LanguageSelector/index.tsx
@@ -4,22 +4,29 @@ import Image from "next/image";
 import deflag from "../../../public/images/icons/deflag.svg";
 import frflag from "../../../public/images/icons/frflag.svg";
 import enflag from "../../../public/images/icons/enflag.svg";
+import { MouseEvent, MouseEventHandler, useState } from "react";
 
 
 const LanguageSelector = () => {
   const router = useRouter();
   const { pathname, asPath, query } = router;
-
+const [openObject, setopenObject] = useState(false)
 
   const getSelectedLanguage = (e: React.MouseEvent<HTMLDivElement>) => {
     console.log("value", e.currentTarget.id);
     router.push({ pathname, query }, asPath, { locale: e.currentTarget.id });
+    setopenObject(!openObject);
   };
+const openLanguage =(event:MouseEvent<HTMLDivElement>)=>{
+  console.log(event);
+  setopenObject(!openObject);
+}
+console.log("openobj",openObject);
 
   return (
     <StyledLangSelector2>
       <div className="langSelector">
-        <div className="langSelectorListNameFlag">
+        <div className="langSelectorListNameFlag" onClick={openLanguage}>
           <div className="flagImgBox">
             <Image
               width={40}
@@ -33,12 +40,12 @@ const LanguageSelector = () => {
             />
             </div>
           <p>
-            <span>
+            <span className={(openObject? "opened":"")}>
               &#9662;
               </span>
           </p>
         </div>
-        <ul className="langSelectorList">
+        <ul className={(openObject? "langSelectorList opened":"langSelectorList")}>
           {router.locales?.filter((local) => { return local !== router.locale }).map((locale) => (
             <li key={locale} value={locale}>
               <div id={locale} onClick={getSelectedLanguage}>
--- src/components/LanguageSelector/styles.ts
+++ src/components/LanguageSelector/styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const StyledLangSelector2 = styled.div`
   margin-top: 10px;
-cursor:pointer;
-margin-right: 20px;
+  cursor: pointer;
+  margin-right: 20px;
 
   .langSelector {
     display: flex;
@@ -11,36 +11,40 @@ margin-right: 20px;
     align-items: center;
     transition: all ease-in 0.15s;
     color: var(--Blue);
-
-    &:hover span{
-  transform: rotate(90deg) ;
+    transform: scale(0.9);
+    &:hover {
+      transform: scale(1);
     }
-
+    
     .langSelectorListNameFlag {
       display: flex;
       align-items: center;
       position: relative;
       
     }
-      .flagImgBox {
-        margin-right: 10px;
-        
-        p {
-          margin-top: -6px;
-        }
+    .flagImgBox {
+      margin-right: 10px;
+
+      p {
+        margin-top: -6px;
       }
-      
-      span{
-        font-size: 34px;
-    position: absolute;
-    margin: 0;
-    padding: 0;
-    top: -9px;
-    right: -30px;
-    color: #757575;
-    transition: all ease-in 0.15s ;
-      }
-      
+    }
+
+    span {
+      font-size: 34px;
+      position: absolute;
+      margin: 0;
+      padding: 0;
+      top: -9px;
+      right: -30px;
+      color: #757575;
+      transition: all ease-in 0.15s;
+    }
+
+    span.opened {
+      transform: rotate(90deg);
+    }
+
     ul.langSelectorList {
       display: flex;
       list-style: none;
@@ -52,30 +56,24 @@ margin-right: 20px;
         width: 0;
         opacity: 0;
         transition: all ease-in 0.1s;
-        &:hover{
-          transform:scale(1.2)
-        }
       }
     }
 
-    &:hover ul.langSelectorList {
+    ul.langSelectorList.opened {
+      li:nth-child(1) {
+        visibility: visible;
+        width: 40px;
+        opacity: 1;
+        transition: all ease-in 0.15s;
+        transition-delay: 50ms;
+      }
 
-    li:nth-child(1) {
-      visibility: visible;
-      width:40px;
-      opacity: 1;
-      transition: all ease-in 0.15s;
-      transition-delay: 50ms;
+      li:nth-child(2) {
+        visibility: visible;
+        width: 40px;
+        opacity: 1;
+        transition: all ease-in 0.15s;
+      }
     }
-    
-    li:nth-child(2) {
-      visibility: visible;
-      width:40px;
-      opacity: 1;
-      transition: all ease-in 0.15s;
-
-    }
-    }
-
   }
 `;
