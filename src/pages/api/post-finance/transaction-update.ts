276bf785b3598bc3e2522129758802619e6bf18b
gutembergg <gmascarenhas3001@gmail.com>
2021-12-02 22:42:32 +0100

    added cors config

--- src/pages/api/post-finance/transaction-update.ts
+++ src/pages/api/post-finance/transaction-update.ts
@@ -3,6 +3,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PostFinanceCheckout } from "postfinancecheckout";
 import { TransactionPending } from "postfinancecheckout/src/models/TransactionPending";
+import initMiddleware from "../../../utils/init-middleware";
+import Cors from "cors";
 
 /* let spaceId: number = 23340;
 let userId: number = 48078;
@@ -18,10 +20,18 @@ let config = {
   api_secret: apiSecret,
 };
 
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
 export default async function handlerUpdate(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await cors(req, res);
+
   const { method } = req;
 
   if (method === "POST") {
--- src/pages/api/post-finance/transaction-validate.ts
+++ src/pages/api/post-finance/transaction-validate.ts
@@ -1,7 +1,9 @@
 "use strict";
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 import { PostFinanceCheckout } from "postfinancecheckout";
+import initMiddleware from "../../../utils/init-middleware";
 
 /* let spaceId: number = 23340;
 let userId: number = 48078;
@@ -17,10 +19,18 @@ let config = {
   api_secret: apiSecret,
 };
 
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
 export default async function handlerValidate(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await cors(req, res);
+
   const { method } = req;
 
   if (method === "POST") {
