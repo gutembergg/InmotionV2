f3fe67708bbc6bb8e23a3e63dff8a048fbad944e
gutembergg <gmascarenhas3001@gmail.com>
2021-12-05 15:08:03 +0100

    wh

--- src/pages/api/post-finance/post-finance-webhooks.ts
+++ src/pages/api/post-finance/post-finance-webhooks.ts
@@ -42,7 +42,7 @@ export default async function handlerCompleted(
       new PostFinanceCheckout.api.TransactionService(config);
 
     transactionService.read(spaceId, dataWebhook.entityId).then((response) => {
-      console.log("orderId: ", response.body.metaData);
+      console.log("orderId: ", response.body.metaData?.orderId);
       if (response.body.state === "AUTHORIZED") {
         console.log("response.AUTHORIZED:8559 ", response.body.state);
         authorizedOrder(8559);
