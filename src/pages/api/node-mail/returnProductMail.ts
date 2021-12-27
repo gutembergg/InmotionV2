66041080ad37b8394f0b6a8c1d3afb09d3c31136
Fatih-esgi <fatih@fat-e.ch>
2021-12-27 15:38:43 +0100

    return product form (disabled until recaptcha installation)

--- src/pages/api/node-mail/returnProductMail.ts
+++ src/pages/api/node-mail/returnProductMail.ts
@@ -1,41 +1,65 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import initMiddleware from "../../../utils/init-middleware";
-import Cors from "cors";
 import { transporter } from "../../../config/nodeMailConfig";
 
-// metre la config -  transporteur
+export default function async(req: NextApiRequest, res: NextApiResponse) {
+  console.log("Req", req);
+  console.log("Res", res);
+  const {
+    address,
+    city,
+    country,
+    date_achat,
+    email,
+    first_name,
+    last_name,
+    message,
+    nom_produit,
+    phone,
+    postcode,
+    revendeur,
+    serial,
+  } = req.body;
 
-const cors = initMiddleware(
-  Cors({
-    methods: ["POST"],
-  })
-);
-
-export default async function returnProducthandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  await cors(req, res);
-  const { method } = req;
-console.log("req--->",req)
-  //si methode post : 
-  if (method === "POST") {
-    const { name, email, message } = req.body;
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: email, // list of receivers
-      subject: "Demande de retour produit Inmotion", // Subject line
-      text: "demande de retour", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-    return res.status(200).json(true)
-  } else {
-    return res.status(400).json({ error: "erreure envoie email" });
+  try {
+    let options = {
+      from: `${email}`,
+      to: "fatih@fat-e.ch",
+      subject: `demande de retour marchandise`,
+      html: `
+<h2>Demande de retour marchandise pour:</h2>
+<b>modèle</b>: ${nom_produit}
+<br />
+<b>sérial:</b> ${serial} 
+<br />
+<b>acheté le :</b> ${date_achat}
+<br />
+<b>chez le revendeur:</b> ${revendeur} <br />
+<br />
+<br />      
+<h2>message du client:</h2>
+${message}
+<br />
+<br />           
+<h2>coordonnées du client:</h2>
+<br />
+<br />
+<b>${first_name} ${last_name}</b>
+<br />
+${address}
+<br />
+${postcode} ${city}
+<br />
+${country}
+<br />
+<br />
+${phone}
+<br />
+${email}
+`,
+    };
+    transporter.sendMail(options);
+    return res.status(200).json(true);
+  } catch (error) {
+    return res.status(400);
   }
 }
-
-
-
-
