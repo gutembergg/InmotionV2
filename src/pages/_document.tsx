837797e03177ea9580f9f6219a675356dd3cd8bb
Fatih-esgi <fatih@fat-e.ch>
2022-02-14 18:45:46 +0100

    fix language in header-> html lang attribute dynamic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
