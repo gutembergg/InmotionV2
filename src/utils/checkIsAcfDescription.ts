75a9e70f31f0eb4beb0a3e1306da0b5aaec3160e
Fatih-esgi <fatih@fat-e.ch>
2022-02-27 19:34:04 +0100

    undo change

--- src/utils/checkIsAcfDescription.ts
+++ src/utils/checkIsAcfDescription.ts
@@ -1,40 +1,19 @@
 import { IProduct } from "../interfaces/IProducts";
 
-// export const checkIsAcfDescription = (product: IProduct) => {
-//   const acfKeys = Object.keys(product.acf);
-//   let productIsAcf = {};
-//   acfKeys.forEach((item) => {
-//     if (item === "description_du_produit") {
-//       const acf = product.acf.description_du_produit;
-//       console.log("acfKeys",acf)
-//       if (Array.isArray(acf)) {
-//         productIsAcf = { ...product, isAcfDescription: true };
-//       }
-//     } else {
-//       productIsAcf = { ...product, isAcfDescription: false };
-//     }
-//   });
-
-//   return productIsAcf;
-// };
-
 export const checkIsAcfDescription = (product: IProduct) => {
   const acfKeys = Object.keys(product.acf);
-
-  let response = { ...product, isAcfDescription: false };
-
+  let productIsAcf = {};
   acfKeys.forEach((item) => {
-    if (item !== "description_du_produit") {
-      return;
+    if (item === "description_du_produit") {
+      const acf = product.acf.description_du_produit;
+      console.log("acfKeys",acf)
+      if (Array.isArray(acf)) {
+        productIsAcf = { ...product, isAcfDescription: true };
+      }
+    } else {
+      productIsAcf = { ...product, isAcfDescription: false };
     }
-    if (item.length === 0) {
-      return;
-    }
-    if (item[0].length === 0) {
-      return;
-    }
-
-    return (response = { ...product, isAcfDescription: true });
   });
-  return response;
+
+  return productIsAcf;
 };
